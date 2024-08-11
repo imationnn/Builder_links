901942de43986a90ab718e2fcc046e9eb0b614d3
roman <imationnn@yandex.ru>
2024-08-11 21:32:46 +0300

    refactoring

--- app/create_links.py
+++ app/create_links.py
@@ -1,6 +1,6 @@
 BASE_URL = "https://www.wildberries.ru{}"
-FILTER_URL = "https://catalog.wb.ru/catalog/{}/v6/filters?appType=1&{}&curr=rub&dest=-1255987&spp=30"
-CATALOG_URL = "https://catalog.wb.ru/catalog/{0}/v2/catalog?appType=1&{1}&curr=rub&dest=-1255987&page={{}}&sort={sort}&spp=30"
+FILTER_URL = "https://catalog.wb.ru/catalog/{}/v6/filters?ab_testing=false&appType=1&{}&curr=rub&dest=-1255987&spp=30"
+CATALOG_URL = "https://catalog.wb.ru/catalog/{0}/v2/catalog?ab_testing=false&appType=1&{1}&curr=rub&dest=-1255987&page={{}}&sort={sort}&spp=30"
 
 
 def create_site_url(path: str) -> str:
