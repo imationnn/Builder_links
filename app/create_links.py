1d2d5c0c8f6368046afb03c8566b8aca45ce00ee
roman <imationnn@yandex.ru>
2024-08-11 15:03:04 +0300

    added methods for create urls

---
+++ app/create_links.py
@@ -0,0 +1,23 @@
+BASE_URL = "https://www.wildberries.ru{}"
+FILTER_URL = "https://catalog.wb.ru/catalog/{}/v6/filters?appType=1&{}&curr=rub&dest=-1255987&spp=30"
+CATALOG_URL = "https://catalog.wb.ru/catalog/{0}/v2/catalog?appType=1&{1}&curr=rub&dest=-1255987&page={{}}&sort={sort}&spp=30"
+
+
+def create_site_url(path: str) -> str:
+    return BASE_URL.format(path)
+
+
+def create_filter_url(shard: str, query: str) -> str:
+    return FILTER_URL.format(shard, query)
+
+
+def create_catalog_url(
+        shard: str,
+        query: str,
+        sort: str,
+        xsubject: str | int | None = None
+) -> str:
+    url = CATALOG_URL.format(shard, query, sort=sort)
+    if xsubject is not None:
+        url = url + f"&xsubject={xsubject}"
+    return url
