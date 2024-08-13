a54c094245d1c8e5f9e9e3697beeb3bdb5528623
roman <imationnn@yandex.ru>
2024-08-13 10:40:11 +0300

    added new methods

--- app/repository/redis_storage.py
+++ app/repository/redis_storage.py
@@ -3,13 +3,17 @@ from typing import Sequence, Literal
 from redis.asyncio import Redis
 
 from app.config.settings import RedisConfig
-from app.schemas.schemas_dto import SubCategoryDTO
-from app.utils import get_translate_category_name
+from app.schemas.schemas_dto import SubCategoryDTO, XsubjectDTO
+from app.utils import (get_translate_category_name,
+                       category_name_to_name_without_underscore,
+                       get_lst_category_name_without_underscore)
 
 
 SUBCATEGORY = "subcat"
 XSUBJECTS = "xsubjects"
 CATALOG = "catalog"
+URLS = "urls"
+REGISTRATION = "registration_of_parsers"
 
 TYPE_CHAPTER = Literal["good", "bad", "neutral"]
 
@@ -106,7 +110,57 @@ class RedisStorage:
         if value:
             value = value.split("|")
             await self._redis.hdel(db_key, subcategory)
-        return value or []
+        return value or ["", ""]
+
+    async def add_xsubjects_to_catalog(
+            self,
+            category_name: str,
+            lst_xsubjects: list[XsubjectDTO]
+    ) -> None:
+        db_key = self.key_builder.build(category_name, XSUBJECTS, CATALOG)
+        data = {i.id: i.name for i in lst_xsubjects}
+        await self._redis.hset(db_key, mapping=data)
+
+    async def get_xsubject_from_catalog(
+            self,
+            category_name: str,
+            xsubject: str | int
+    ) -> str:
+        db_key = self.key_builder.build(category_name, XSUBJECTS, CATALOG)
+        xsubject = str(xsubject)
+        value = await self._redis.hget(db_key, xsubject)
+        if value:
+            await self._redis.hdel(db_key, xsubject)
+        return value or ""
+
+    async def add_urls(self, category_name: str, *urls: str | Sequence[str]) -> None:
+        db_key = self.key_builder.build(category_name, URLS)
+        await self._redis.sadd(db_key, *urls)
+
+    async def get_urls(self, category_name: str) -> set:
+        db_key = self.key_builder.build(category_name_to_name_without_underscore(category_name), URLS)
+        return await self._redis.smembers(db_key)
+
+    async def delete_urls(self, category_name: str) -> None:
+        db_key = self.key_builder.build(category_name, URLS)
+        await self._redis.delete(db_key)
+
+    async def add_parser_to_register(self, category: str | list[str], url: str) -> None:
+        if isinstance(category, str):
+            category = [category]
+        data = {i: url for i in get_lst_category_name_without_underscore(category)}
+        await self._redis.hset(REGISTRATION, mapping=data)
+
+    async def get_parser_url(self, category_name: str) -> str:
+        return await self._redis.hget(REGISTRATION, category_name)
+
+    async def delete_parser_from_register(self, category: str | list[str]) -> None:
+        if isinstance(category, str):
+            category = [category]
+        await self._redis.hdel(REGISTRATION, *get_lst_category_name_without_underscore(category))
+
+    async def get_categories_for_parsing(self) -> list:
+        return await self._redis.hkeys(REGISTRATION)
 
     async def close(self) -> None:
         await self._redis.aclose()
