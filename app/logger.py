7851a6547e0f1faabc009a3d364a0f84ba4ce418
roman <imationnn@yandex.ru>
2024-08-13 19:10:48 +0300

    refactoring

--- app/logger.py
+++ app/logger.py
@@ -4,7 +4,7 @@ from logging.handlers import RotatingFileHandler
 
 def configure_logging(write_logger_level, stream_logger_level):
     form = '%(levelname)-8s [%(asctime)s] - %(filename)s:%(lineno)d - %(message)s'
-    rothand = RotatingFileHandler(filename='logs/log.log', maxBytes=1000000, backupCount=10)
+    rothand = RotatingFileHandler(filename='app/logs/log.log', maxBytes=1000000, backupCount=10)
     rothand.setFormatter(logging.Formatter(fmt=form))
     rothand.setLevel(write_logger_level)
     stream = logging.StreamHandler()
