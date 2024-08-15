7a379554ab6287667b5db785ea2cc5e95102549c
roman <imationnn@yandex.ru>
2024-08-15 19:37:31 +0300

    added new settings and env.example

--- app/config/settings.py
+++ app/config/settings.py
@@ -10,3 +10,15 @@ class RedisConfig(BaseSettings):
     redis_port: int
     redis_db_name: str
     redis_password: str | None = None
+
+
+class Settings(BaseSettings):
+    min_count_for_sort: int
+    max_count_for_sort: int
+    min_count_for_filter: int
+    chunk_size: int
+    sleep_between_request: int | float
+    sleep_between_chunk: int | float
+
+
+settings = Settings()
