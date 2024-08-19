c919df758dd3de057888c465cfcdffa5eb7fc124
roman <imationnn@yandex.ru>
2024-08-19 20:49:14 +0300

    added notify config

--- app/config/settings.py
+++ app/config/settings.py
@@ -12,6 +12,19 @@ class RedisConfig(BaseSettings):
     redis_password: str | None = None
 
 
+class NotifyConfig(BaseSettings):
+    notify_host: str
+    notify_port: int
+    notify_path: str
+    protocol: str = "http"
+
+    @property
+    def notify_url(self):
+        if self.notify_port in [443, 8443]:
+            self.protocol = "https"
+        return f"{self.protocol}://{self.notify_host}:{self.notify_port}{self.notify_path}"
+
+
 class Settings(BaseSettings):
     min_count_for_sort: int
     max_count_for_sort: int
@@ -19,6 +32,9 @@ class Settings(BaseSettings):
     chunk_size: int
     sleep_between_request: int | float
     sleep_between_chunk: int | float
+    use_notify_send: bool
+    use_notify_log: bool
+    notify_config: NotifyConfig = NotifyConfig()
 
 
 settings = Settings()
